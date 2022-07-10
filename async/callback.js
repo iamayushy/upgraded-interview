8c038c2e46f99defbf74269ac38a4fdd2b688d58
iamayushy <ayushhuns@gmail.com>
2022-07-10 21:02:51 +0530

    flix

---
+++ async/callback.js
@@ -0,0 +1,19 @@
+// Any function that is passed as an argument to another function
+// is called as callback function
+
+/*
+1. They can be synchronous or async
+2. Used to fetch data
+3. Issue of callback hell
+
+*/
+
+/*
+A function which accepts a function as an arguments
+or returns a fuction is called a higher order function
+*/
+
+/*
+if the function depends on the older function then it gets
+deeply nested in order to solve it use promises.
+*/
